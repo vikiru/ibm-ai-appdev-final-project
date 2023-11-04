b0718f03a14b105ac3768b444ac6a07eb8a79d71
vikiru <72267229+vikiru@users.noreply.github.com>
2023-11-04 18:44:13 -0400

    Complete Task 2

---
+++ final_project/emotion_detection.py
@@ -0,0 +1,8 @@
+import requests
+
+def emotion_detector(text_to_analyse):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_json = {"raw_document": {"text": text_to_analyse}}
+    response = requests.post(url, json=input_json, headers=header)
+    return response.text
