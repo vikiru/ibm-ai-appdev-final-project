0c7f20e97cd5df47cd4dce3a51e6353f70ba443b
vikiru <72267229+vikiru@users.noreply.github.com>
2023-11-06 16:27:21 -0500

    Commit Task 3

--- final_project/emotion_detection.py
+++ final_project/emotion_detection.py
@@ -1,8 +1,13 @@
 import requests
+import json
 
-def emotion_detector(text_to_analyse):
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+def emotion_detector(text_to_analyze):
+    url ='https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    input_json = {"raw_document": {"text": text_to_analyse}}
+    input_json = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json=input_json, headers=header)
-    return response.text
+    formatted_response = json.loads(response.text)
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    dominant_emotion = max(emotions.items(), key=lambda x: x[1])
+    emotions['dominant_emotion'] = dominant_emotion[0]
+    return emotions
